2080a876f6146988e9346a89af009b9ad46c6d17
GUNAKALA AMARNADH <77877354+amarnadh-EMP@users.noreply.github.com>
2021-02-18 18:44:28 +0530

    Update Parsing_JSON.py

--- Parsing_JSON.py
+++ Parsing_JSON.py
@@ -1,3 +1,6 @@
+"""Parsing JSON This code corrects JSON which has comma delimiter is missing. 
+adding your input file path as source and get output in form of file path"""
+
 
 with open("Examples\Mashup20210218.000000000.JSON") as f:
    file= f.readlines()
