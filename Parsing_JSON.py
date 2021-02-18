6a7f2c090fa0f0817a9cc102e537aa419a78db38
GUNAKALA AMARNADH <77877354+amarnadh-EMP@users.noreply.github.com>
2021-02-18 18:43:41 +0530

    Update Parsing_JSON.py

--- Parsing_JSON.py
+++ Parsing_JSON.py
@@ -3,6 +3,6 @@ with open("Examples\Mashup20210218.000000000.JSON") as f:
    file= f.readlines()
 
 
-with open("Log.txt","a+") as log:
+with open("JSON_Format.txt","a+") as log:
     for i in file:  
         log.write(i.replace('}', '},')) 
